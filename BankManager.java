19cf1fd9ef9c2467ac184fdfab30a7823940ea10
Aniket Gupta <trustaniket@gmail.com>
2020-11-23 22:44:58 +0530

    Update BankManager.java

--- BankManager.java
+++ BankManager.java
@@ -3,42 +3,42 @@ import java.util.Scanner;
 
 public class BankManager{
 
-public static void main(String[] args) throws IOException{
-Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
 
-int t = sc.nextInt();
+        int t = sc.nextInt();
 
-while(t --> 0){
-int n = sc.nextLong();
-int count = 0;
-if(n == 1){
-System.out.println(1);
-continue;
-}
-else{
-while(n % 2 == 0){
-count += 2;
-n /= 2;
-}
-for (int i = 3; i <= Math.sqrt(n); i=i+2){
-while(n % i == 0){
-count += i;
-n /= i;
-}
-}
-if (n > 2)
-  count += n;
+        while(t --> 0){
+            long n = sc.nextLong();
+            int count = 0;
+            if(n == 1){
+                System.out.println(1);
+                continue;
+            }
+            else{
+                while(n % 2 == 0){
+                    count += 2;
+                    n /= 2;
+                }
+                for (int i = 3; i <= Math.sqrt(n); i=i+2){
+                    while(n % i == 0){
+                        count += i;
+                        n /= i;
+                    }
+                }
+                if (n > 2)
+                    count += n;
 
-System.out.println(count);
-continue;
-
-
-
-}
-
-}
+                System.out.println(count);
+                continue;
 
 
-}
-  
+
+            }
+
+        }
+
+
+    }
+
 }
